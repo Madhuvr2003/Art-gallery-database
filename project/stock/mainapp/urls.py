f8f8f4ac87221b5f6f71a09c763f7674f3df4863
Madhu V R <madhuvr271@gmail.com>
2024-08-07 13:28:04 +0530

    Delete project/stock directory

--- project/stock/mainapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path("",views.basic,name="basic"),
-    path('stockpicker', views.stockpicker, name = 'stockpicker'),
-    path('stocktracker/', views.stocktracker, name = 'stocktracker'),
-]
